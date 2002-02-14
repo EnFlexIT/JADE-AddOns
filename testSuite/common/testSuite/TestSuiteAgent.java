05562ecea3d6abe400917436c2d7a3a677a835bd
caire <caire@815794fe-97f2-0310-9206-e62f04a60ddc>
2002-02-14 11:06:11 +0000

    Added a "test-suite" gui to control the execution of the tests
    git-svn-id: https://jade.tilab.com/svn/jade_add-ons/trunk@55 815794fe-97f2-0310-9206-e62f04a60ddc

---
+++ testSuite/common/testSuite/TestSuiteAgent.java
@@ -0,0 +1,136 @@
+/*****************************************************************
+JADE - Java Agent DEvelopment Framework is a framework to develop
+multi-agent systems in compliance with the FIPA specifications.
+Copyright (C) 2000 CSELT S.p.A. 
+
+GNU Lesser General Public License
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, 
+version 2.1 of the License. 
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA  02111-1307, USA.
+*****************************************************************/
+
+package test.common.testSuite;
+
+import jade.core.Agent;
+import jade.core.AID;
+import jade.core.Runtime;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.behaviours.*;
+import jade.lang.acl.*;
+import jade.wrapper.*;
+import jade.gui.GuiAgent;
+import jade.gui.GuiEvent;
+
+import test.common.*;
+import test.common.testSuite.gui.*;
+
+public class TestSuiteAgent extends GuiAgent {
+	private static final String NAME = "test-suite";
+	private static final String TESTER_NAME = "tester";
+	
+	// Gui event types
+	public static final int EXIT_EVENT = 0;
+	public static final int START_TESTER_EVENT = 1;
+	public static final int STEP_EVENT = 2;
+	
+	private AgentContainer containerController;
+	private TestSuiteGui myGui;
+	private MessageTemplate testerExitNotificationTemplate = MessageTemplate.MatchContent(TesterAgent.TESTER_EXITED);
+	
+	TestSuiteAgent(AgentContainer containerController) {
+		super();
+		this.containerController = containerController;
+	}
+	
+	protected void setup() {	
+		myGui = new TestSuiteGui(this, new String[] {
+			"test.content.ContentTesterAgent",
+			"test.interPlatform.InterPlatformCommunicationTesterAgent",
+			"test.domain.JADEManagementOntologyTesterAgent",
+			"test.proto.ContractNetTesterAgent",
+			"test.proto.AchieveRETesterAgent" } );
+		myGui.showCorrect();		
+		
+		// This behaviour receives exit notifications from the tester agents
+		addBehaviour(new CyclicBehaviour() {
+			public void action() {
+				ACLMessage msg = receive(testerExitNotificationTemplate);
+				if (msg != null) {
+					myGui.setStatus(TestSuiteGui.READY_STATE);
+				}
+				else {
+					block();
+				}
+			}
+		} );
+	}	
+		
+	protected void takeDown() {
+		myGui.dispose();
+	}
+	
+	protected void onGuiEvent(GuiEvent ev) {
+		switch (ev.getType()) {
+		case START_TESTER_EVENT:
+			try {
+				TesterAgent ta = (TesterAgent) ev.getParameter(0);
+  	    AgentController tester = containerController.acceptNewAgent(TESTER_NAME, ta);
+  	    tester.start();
+			}
+			catch (Exception e) {
+				System.out.println("Error starting tester agent. "+e);
+				myGui.setStatus(TestSuiteGui.IDLE_STATE);
+			}
+			break;
+		case STEP_EVENT:
+			ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+			msg.addReceiver(new AID(TESTER_NAME, AID.ISLOCALNAME));
+			msg.setContent(TesterAgent.STEP);
+			send(msg);
+			break;
+		case EXIT_EVENT:
+			doDelete();
+			break;
+		}
+	}
+	
+	// Main method that allows launching the TestSuiteAgent as a 
+	// stand-alone program 	
+	public static void main(String[] args) {
+		try {
+      // Get a hold on JADE runtime
+      Runtime rt = Runtime.instance();
+
+      // Exit the JVM when there are no more containers around
+      rt.setCloseVM(true);
+
+      Profile pMain = new ProfileImpl(null, 8888, null);
+
+      MainContainer mc = rt.createMainContainer(pMain);
+
+      AgentController rma = mc.createNewAgent("rma", "jade.tools.rma.rma", new Object[0]);
+      rma.start();
+
+      TestSuiteAgent tsa = new TestSuiteAgent(mc);
+      AgentController testSuite = mc.acceptNewAgent(NAME, tsa);
+      testSuite.start();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+ 
+}
---
+++ testSuite/common/testSuite/gui/TestSuiteGui.java
@@ -0,0 +1,224 @@
+/*****************************************************************
+JADE - Java Agent DEvelopment Framework is a framework to develop
+multi-agent systems in compliance with the FIPA specifications.
+Copyright (C) 2000 CSELT S.p.A. 
+
+GNU Lesser General Public License
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, 
+version 2.1 of the License. 
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA  02111-1307, USA.
+*****************************************************************/
+
+package test.common.testSuite.gui;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.StringTokenizer;
+
+import jade.gui.GuiAgent;
+import jade.gui.GuiEvent;
+import jade.util.leap.ArrayList;
+
+import test.common.TesterAgent;
+import test.common.testSuite.TestSuiteAgent;
+
+public class TestSuiteGui extends JFrame {
+	// Gui states used to enable/disable buttons
+	public static final int IDLE_STATE = 0;
+	public static final int READY_STATE = 1;
+	public static final int RUNNING_STATE = 2;
+	public static final int DEBUGGING_STATE = 3;
+	private int status;
+	
+	private GuiAgent myAgent;
+	private String[] testers;
+	private String currentTester = null;
+	
+	private JButton exitB, openB, runB, debugB, stepB;
+	private JMenuItem exitI, openI, runI, debugI, stepI;
+	private JTextField argsF;
+	
+	public TestSuiteGui(GuiAgent myAgent, String[] testers) {
+		super();
+		this.myAgent = myAgent;
+		this.testers = testers;
+		
+		setTitle("JADE Test Suite toolbar");
+		
+    Icon exitImg = GuiProperties.getIcon("exit");
+    Icon openImg = GuiProperties.getIcon("open");
+		Icon runImg = GuiProperties.getIcon("run");
+		Icon debugImg = GuiProperties.getIcon("debug");
+    Icon stepImg = GuiProperties.getIcon("step");
+    
+		/////////////////////////////////////////////////////
+		// Add Toolbar to the NORTH part of the border layout 
+		JToolBar bar = new JToolBar();
+
+		// TEST
+		exitB  = bar.add(new ExitAction(this));
+		exitB.setText("");
+		exitB.setIcon(exitImg);
+		//exitB.setDisabledIcon(exitImg);
+		exitB.setToolTipText("Exit the JADE Test Suite");
+		
+		bar.addSeparator();
+		
+		openB  = bar.add(new OpenAction(this));
+		openB.setText("");
+		openB.setIcon(openImg);
+		//runB.setDisabledIcon(runImg);
+		openB.setToolTipText("Load a tester agent");
+		
+		runB  = bar.add(new RunAction(this));
+		runB.setText("");
+		runB.setIcon(runImg);
+		//runB.setDisabledIcon(runImg);
+		runB.setToolTipText("Run the current tester agent");
+		
+		bar.addSeparator();
+		
+		// DEBUG
+		debugB  = bar.add(new DebugAction(this));
+		debugB.setText("");
+		debugB.setIcon(debugImg);
+		//goB.setDisabledIcon(debugImg);
+		debugB.setToolTipText("Debug the current tester agent");
+		
+		stepB  = bar.add(new StepAction(this));
+		stepB.setText("");
+		stepB.setIcon(stepImg);
+		//stepB.setDisabledIcon(stepImg);
+		stepB.setToolTipText("Execute next test");
+		
+		bar.addSeparator();
+		
+		argsF = new JTextField(20);
+		bar.add(argsF);
+		
+		getContentPane().add(bar, BorderLayout.NORTH);
+
+		// Execute the EXIT action when the user attempts to close 
+		// the JADE Test Suite GUI using the button on the upper right corner	
+		addWindowListener(new	WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				ExitAction ac = new ExitAction(TestSuiteGui.this);
+				ac.actionPerformed(new ActionEvent((Object) this, 0, "Exit"));
+			}
+		} );
+		
+		setResizable(false);
+		setStatus(IDLE_STATE);
+	}
+	
+	public void showCorrect() {
+		pack();
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int centerX = (int)screenSize.getWidth() / 2;
+		int centerY = (int)screenSize.getHeight() / 2;
+		setLocation(centerX - getWidth() / 2, centerY - getHeight() / 2);
+		show();
+	}
+	
+	void runTester() {
+		TesterAgent tester = loadTester();
+		if (tester != null) {
+			setStatus(RUNNING_STATE);
+			tester.setDebugMode(false);
+			GuiEvent ev = new GuiEvent(this, TestSuiteAgent.START_TESTER_EVENT); 
+			ev.addParameter(tester);
+			myAgent.postGuiEvent(ev);
+		}
+	}
+	
+	void debugTester() {
+		TesterAgent tester = loadTester();
+		if (tester != null) {
+			setStatus(DEBUGGING_STATE);
+			tester.setDebugMode(true);
+			GuiEvent ev = new GuiEvent(this, TestSuiteAgent.START_TESTER_EVENT); 
+			ev.addParameter(tester);
+			myAgent.postGuiEvent(ev);
+		}
+	}
+	
+	void exit() {
+		myAgent.postGuiEvent(new GuiEvent(this, TestSuiteAgent.EXIT_EVENT)); 
+	}
+	
+	void step() {
+		myAgent.postGuiEvent(new GuiEvent(this, TestSuiteAgent.STEP_EVENT)); 
+	}
+		
+	void selectTester() {
+		GenericChooser c = new GenericChooser();
+		int i = c.showChoiceDlg(this,
+						"Select the tester agent to run",
+						"OK",
+						"Cancel",
+						testers);
+		if (i >= 0) {
+			currentTester = testers[i];
+		}
+		
+		if (currentTester != null) {
+			setStatus(READY_STATE);
+		}
+		else {
+			setStatus(IDLE_STATE);
+		}
+	}
+	
+	private Object[] getArguments() {
+		ArrayList l = new ArrayList();
+		String args = argsF.getText().trim();
+		if (args != null && (!args.equals(""))) {
+			StringTokenizer st = new StringTokenizer(args, " ");
+			while (st.hasMoreTokens()) {
+				String s = st.nextToken();
+ 				l.add(s);
+			}
+		}
+		return l.toArray();
+	}
+	
+	private TesterAgent loadTester() {
+		try {
+			TesterAgent ta = (TesterAgent) Class.forName(currentTester).newInstance();
+			ta.setArguments(getArguments());
+			ta.setTestSuiteExecution(myAgent.getLocalName());
+			return ta;
+		}
+		catch (Exception e) {
+			System.out.println("Error loading tester agent. "+e);
+			return null;
+		}
+	}
+		
+	public void setStatus(int status) {
+		this.status = status;
+		updateEnabled();
+	}
+	
+	private void updateEnabled() {
+		openB.setEnabled(status == IDLE_STATE || status == READY_STATE);
+		runB.setEnabled(status == READY_STATE);
+		debugB.setEnabled(status == READY_STATE);
+		stepB.setEnabled(status == DEBUGGING_STATE);
+		argsF.setEnabled(status == READY_STATE);
+	}
+	
+}
