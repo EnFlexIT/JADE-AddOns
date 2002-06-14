a96e9108d43e61734990cd58404abe4a1b96541b
vitaglione <vitaglione@815794fe-97f2-0310-9206-e62f04a60ddc>
2002-06-14 06:55:52 +0000

    Now Digest.digest() can calculate the hash of a string also using the DES-based algorithm. This is needed in order to use a Linux password file as Jade password file. Modified the command-line parameters order in the method main().
    git-svn-id: https://jade.tilab.com/svn/jade_add-ons/trunk@73 815794fe-97f2-0310-9206-e62f04a60ddc

--- security/src/jade/security/impl/Digest.java
+++ security/src/jade/security/impl/Digest.java
@@ -2,7 +2,7 @@ package jade.security.impl;
 
 import java.security.MessageDigest;
 import java.math.BigInteger;
-
+import jade.security.impl.DES.Crypt;
 
 public class Digest {
 	
@@ -10,7 +10,28 @@ public class Digest {
 		return digest(message.getBytes(), algorithm);
 	}
 
+	/*
+	 *  returns the DES digest of 'message' using 'salt'
+	 *  'algorithm' here can be only "DES"
+	 *
+	 */
+	public static String digest(byte[] message, String algorithm, String salt) {
+ 		if (algorithm.compareTo("DES")!=0) { 
+			return "only-DES";
+		}
+		// Calculate DES "hash" with the provided key
+		return jade.security.impl.DES.Crypt.crypt( salt, new String(message) ); 
+
+	} // end digest (byte[], String, String) for DES
+
+
+	/*
+	 *  returns the digest of 'message' using a specified 'algorithm'
+	 *  The provided algorithm has to be supported in JCE
+	 *
+	 */
 	public static String digest(byte[] message, String algorithm) {
+		// Use a hashing function supported in Java
 		try {
 			MessageDigest md = MessageDigest.getInstance(algorithm);
 			md.reset();
@@ -22,14 +43,25 @@ public class Digest {
 			e.printStackTrace();
 		}
 		return "###";
-	}
+	} // end digest()
+
+
 
 	public static void main(String[] args) {
-		if (args.length > 1)
-			System.out.println(digest(args[0], args[1]));
-		else if (args.length > 0)
-			System.out.println(digest(args[0], "MD5"));
-		else
-			System.out.println("usage: java jade.security.impl.Digest <message> [<algorithm>]");
-	}
-}
+		if ( args[0].compareTo("DES")==0 ) {
+			System.out.println(digest( args[0].getBytes(),  args[1], "DES"));
+		} else { 
+			if (args.length > 1) {
+				System.out.println( digest(args[1], args[0] ));
+			} else {
+				System.out.println("\nusage: java jade.security.impl.Digest <algorithm> <message> [<salt>]\n");
+				System.out.println(" Examples: ");
+				System.out.println("   java jade.security.impl.Digest MD5 password");
+				System.out.println("   java jade.security.impl.Digest DES password sa");
+			}
+		}
+	} // end main()
+} // end class
+
+
+
