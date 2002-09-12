b220d2702262f4ba511e3c0b649583a4398bf259
vitaglione <vitaglione@815794fe-97f2-0310-9206-e62f04a60ddc>
2002-09-12 15:45:53 +0000

    Source files for the example: 'send': a very simple example about security permissions for messaging.
    git-svn-id: https://jade.tilab.com/svn/jade_add-ons/trunk@118 815794fe-97f2-0310-9206-e62f04a60ddc

---
+++ security/src/examples/security/send/Barbara.java
@@ -0,0 +1,64 @@
+/*****************************************************************
+JADE - Java Agent DEvelopment Framework is a framework to develop 
+multi-agent systems in compliance with the FIPA specifications.
+Copyright (C) 2002 TILAB S.p.A. 
+
+GNU Lesser General Public License
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, 
+version 2.1 of the License. 
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA  02111-1307, USA.
+*****************************************************************/
+
+package examples.security.send;
+
+
+import jade.core.*;
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
+
+
+
+/**
+ *
+ *
+ *
+@author Giosuè Vitaglione - TILAB S.p.A.
+@version  $Date$ $Revision$  
+ */
+public class Barbara extends Agent {
+
+
+  protected void setup() {
+
+	addBehaviour(new SimpleBehaviour(this) {
+
+		int count=10;
+
+		public void action() {
+			ACLMessage msg = blockingReceive();
+		    System.out.println( "\n		Barbara:  Received message from "+msg.getSender().getName() );
+		    System.out.println( "				  "+msg.getContent() );
+		} // end action
+		
+		public boolean done() {
+			return false;
+		}
+		
+	}); // end Behaviour
+
+  } // end setup
+  
+  
+}//end class Anja
