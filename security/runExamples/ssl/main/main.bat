54d1561b99cdf3aa507daeb6781a52721a5b44ac
vitaglione <vitaglione@815794fe-97f2-0310-9206-e62f04a60ddc>
2002-09-13 15:13:29 +0000

    'ssl' example files.
    git-svn-id: https://jade.tilab.com/svn/jade_add-ons/trunk@125 815794fe-97f2-0310-9206-e62f04a60ddc

---
+++ security/runExamples/ssl/main/main.bat
@@ -0,0 +1,5 @@
+set JADE_HOME=..\..\..\..\..\
+set MYCLPATH=..\..\..\lib\jadeS.jar;..\..\classes\;%JADE_HOME%\lib\jade.jar;%JADE_HOME%\lib\jadeTools.jar;%JADE_HOME%\lib\iiop.jar;
+java -classpath %MYCLPATH% jade.Boot -gui -conf main.conf -owner alice:wonnapass da0:jade.tools.DummyAgent.DummyAgent
+
+
