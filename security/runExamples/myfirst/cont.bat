12283471fa04cb1b2441752a3506313818458c3b
vitaglione <vitaglione@815794fe-97f2-0310-9206-e62f04a60ddc>
2002-09-13 15:11:01 +0000

    'myfirst' example files.
    git-svn-id: https://jade.tilab.com/svn/jade_add-ons/trunk@123 815794fe-97f2-0310-9206-e62f04a60ddc

---
+++ security/runExamples/myfirst/cont.bat
@@ -0,0 +1,5 @@
+set JADE_HOME=..\..\..\..\
+set MYCLPATH=..\..\lib\jadeS.jar;..\..\classes\;%JADE_HOME%\lib\jade.jar;%JADE_HOME%\lib\jadeTools.jar;%JADE_HOME%\lib\iiop.jar; 
+java -classpath %MYCLPATH% -Djava.security.manager -Djava.security.policy=basic.policy jade.Boot -container -owner alice:wannapass da1:jade.tools.DummyAgent.DummyAgent 
+
+
