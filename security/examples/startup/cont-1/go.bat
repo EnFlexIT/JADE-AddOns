38e556a525128fe619a1649d4448227c302d89bc
vitaglione <vitaglione@815794fe-97f2-0310-9206-e62f04a60ddc>
2004-07-20 10:51:15 +0000

    easy start.
    git-svn-id: https://jade.tilab.com/svn/jade_add-ons/trunk@448 815794fe-97f2-0310-9206-e62f04a60ddc

---
+++ security/examples/startup/cont-1/go.bat
@@ -0,0 +1,10 @@
+set JADEROOT=../../../../..
+set LIBJADEDIR=%jaderoot%\lib
+
+
+set MYCLPATH=%LIBJADEDIR%\jade.jar;%LIBJADEDIR%\jadeTools.jar;%LIBJADEDIR%\Base64.jar;%JADEROOT%\add-ons\security\lib\jadeSecurity.jar;
+
+java -cp %MYCLPATH% jade.Boot -conf cont-1.conf -container -host 127.0.0.1
+
+
+
