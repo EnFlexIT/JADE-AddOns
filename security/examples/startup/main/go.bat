5fefd69cadffee3ced271780fa18d84f46dc17c3
vitaglione <vitaglione@815794fe-97f2-0310-9206-e62f04a60ddc>
2004-07-20 10:51:00 +0000

    fixed classpath
    git-svn-id: https://jade.tilab.com/svn/jade_add-ons/trunk@447 815794fe-97f2-0310-9206-e62f04a60ddc

--- security/examples/startup/main/go.bat
+++ security/examples/startup/main/go.bat
@@ -2,7 +2,7 @@ set JADEROOT=../../../../..
 set LIBJADEDIR=%jaderoot%\lib
 
 
-set MYCLPATH=%LIBJADEDIR%\jade.jar;%LIBJADEDIR%\jadeTools.jar;%JADEROOT%\add-ons\security\lib\jadeSecurity.jar;
+set MYCLPATH=%LIBJADEDIR%\jade.jar;%LIBJADEDIR%\jadeTools.jar;%LIBJADEDIR%\Base64.jar;%JADEROOT%\add-ons\security\lib\jadeSecurity.jar;
 
 java -cp %MYCLPATH% jade.Boot -conf main.conf
 
