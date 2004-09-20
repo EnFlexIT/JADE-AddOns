f70b0be522aebb171ca06feade932a3dd1592590
vitaglione <vitaglione@815794fe-97f2-0310-9206-e62f04a60ddc>
2004-09-20 08:01:02 +0000

    Use user.language system property, vm messages appear in english.
    git-svn-id: https://jade.tilab.com/svn/jade_add-ons/trunk@507 815794fe-97f2-0310-9206-e62f04a60ddc

--- security/examples/exitAgent/go.bat
+++ security/examples/exitAgent/go.bat
@@ -2,7 +2,7 @@ set JADEROOT=../../../..
 set LIBJADEDIR=%jaderoot%\lib
 set MYCLPATH=%LIBJADEDIR%\jade.jar;%LIBJADEDIR%\Base64.jar;%LIBJADEDIR%\jadeTools.jar;%JADEROOT%\add-ons\security\lib\jadeSecurity.jar;%JADEROOT%\add-ons\security\lib\examples.jar;
 
-java -cp %MYCLPATH%  jade.Boot -conf main.conf
+java -Duser.language=en -cp %MYCLPATH%  jade.Boot -conf main.conf
 
 
 pause
