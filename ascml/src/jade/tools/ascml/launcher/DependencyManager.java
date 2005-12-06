df96e246a65e4f83171a2f22e408876066bebcfd
ascml <ascml@815794fe-97f2-0310-9206-e62f04a60ddc>
2005-12-06 17:06:32 +0000

    git-svn-id: https://jade.tilab.com/svn/jade_add-ons/trunk@607 815794fe-97f2-0310-9206-e62f04a60ddc

---
+++ ascml/src/jade/tools/ascml/launcher/DependencyManager.java
@@ -0,0 +1,11 @@
+package jade.tools.ascml.launcher;
+
+public class DependencyManager {
+	
+	private AgentLauncher myAgentLauncher;
+
+	public DependencyManager(AgentLauncher launcher) {
+		myAgentLauncher=launcher;
+	}
+
+}
