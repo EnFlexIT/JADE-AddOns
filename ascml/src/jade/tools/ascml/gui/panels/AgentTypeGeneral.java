85b47dd6f1a5e7ed490e3e277f16ae197b0d91cc
ascml <ascml@815794fe-97f2-0310-9206-e62f04a60ddc>
2006-02-24 17:43:28 +0000

    now applies model-changes
    git-svn-id: https://jade.tilab.com/svn/jade_add-ons/trunk@748 815794fe-97f2-0310-9206-e62f04a60ddc

--- ascml/src/jade/tools/ascml/gui/panels/AgentTypeGeneral.java
+++ ascml/src/jade/tools/ascml/gui/panels/AgentTypeGeneral.java
@@ -29,21 +29,27 @@ import javax.swing.*;
 import java.awt.*;
 
 import java.awt.event.*;
+import java.io.File;
+
 import jade.tools.ascml.gui.dialogs.StartAgentInstanceDialog;
 import jade.tools.ascml.repository.loader.ImageIconLoader;
 import jade.tools.ascml.absmodel.IAgentType;
 
 public class AgentTypeGeneral extends AbstractPanel implements ActionListener
 {
+    private JLabel iconLabel;
+
 	private JButton buttonApply;
     private JButton buttonSave;
 	private JButton buttonStart;
 
 	private JButton buttonChangeImage;
 	private JButton buttonChangeSourcePath;
+	private JButton buttonChangeSourceName;
 	private JButton buttonChangeTypeClass;
 
 	private JTextField textFieldSourcePath;
+	private JTextField textFieldSourceName;
 	private JTextField textFieldTypeName;
 	private JTextField textFieldTypePackage;
 	private JTextField textFieldTypeClass;
@@ -51,7 +57,7 @@ public class AgentTypeGeneral extends AbstractPanel implements ActionListener
 	private JTextArea textAreaDescription;
 	private JComboBox comboBoxPlatform;
 
-
+    private String iconName;
 	private IAgentType model;
 
 	/**
@@ -97,29 +103,35 @@ public class AgentTypeGeneral extends AbstractPanel implements ActionListener
 	private JPanel createAttributePanel()
 	{
 		// first create all the components
+		iconLabel = new JLabel(ImageIconLoader.scaleImageIcon(model.getIcon(), 32, 32));
+        iconName = model.getIconName();
 
-		textFieldSourcePath = new JTextField(model.getDocument().getSourcePath(), 20);
+		textFieldSourcePath = new JTextField(model.getDocument().getSourcePath(), 30);
 		textFieldSourcePath.setMinimumSize(new Dimension(320, (int)textFieldSourcePath.getPreferredSize().getHeight()));
 		textFieldSourcePath.setBackground(Color.WHITE);
 
-		textFieldTypeName = new JTextField(model.getName(), 20);
+		textFieldSourceName = new JTextField(model.getDocument().getSourceName(), 30);
+		textFieldSourceName.setMinimumSize(new Dimension(320, (int)textFieldSourceName.getPreferredSize().getHeight()));
+		textFieldSourceName.setBackground(Color.WHITE);
+
+		textFieldTypeName = new JTextField(model.getName(), 30);
 		textFieldTypeName.setBackground(Color.WHITE);
 		textFieldTypeName.setMinimumSize(new Dimension(320, (int)textFieldTypeName.getPreferredSize().getHeight()));
 
-		textFieldTypePackage = new JTextField(model.getPackageName(), 20);
+		textFieldTypePackage = new JTextField(model.getPackageName(), 30);
 		textFieldTypePackage.setBackground(Color.WHITE);
 		textFieldTypePackage.setMinimumSize(new Dimension(320, (int)textFieldTypePackage.getPreferredSize().getHeight()));
 
-		textFieldTypeClass = new JTextField(model.getClassName(), 20);
+		textFieldTypeClass = new JTextField(model.getClassName(), 30);
 		textFieldTypeClass.setBackground(Color.WHITE);
 		textFieldTypeClass.setMinimumSize(new Dimension(320, (int)textFieldTypeClass.getPreferredSize().getHeight()));
 
-		buttonChangeImage = new JButton("Change");
+		buttonChangeImage = new JButton("Change Icon");
 		buttonChangeImage.addActionListener(this);
 		buttonChangeImage.setMargin(new Insets(1,1,1,1));
-		buttonChangeImage.setPreferredSize(new Dimension(60,18));
-		buttonChangeImage.setMinimumSize(new Dimension(60,18));
-		buttonChangeImage.setMaximumSize(new Dimension(60,18));
+		buttonChangeImage.setPreferredSize(new Dimension(80,18));
+		buttonChangeImage.setMinimumSize(new Dimension(80,18));
+		buttonChangeImage.setMaximumSize(new Dimension(80,18));
 
 		buttonChangeSourcePath = new JButton("...");
 		buttonChangeSourcePath.addActionListener(this);
@@ -127,6 +139,13 @@ public class AgentTypeGeneral extends AbstractPanel implements ActionListener
 		buttonChangeSourcePath.setPreferredSize(new Dimension(30,20));
 		buttonChangeSourcePath.setMinimumSize(new Dimension(30,20));
 		buttonChangeSourcePath.setMaximumSize(new Dimension(30,20));
+
+		buttonChangeSourceName = new JButton("...");
+		buttonChangeSourceName.addActionListener(this);
+		buttonChangeSourceName.setMargin(new Insets(1,3,1,3));
+		buttonChangeSourceName.setPreferredSize(new Dimension(30,20));
+		buttonChangeSourceName.setMinimumSize(new Dimension(30,20));
+		buttonChangeSourceName.setMaximumSize(new Dimension(30,20));
 
 		buttonChangeTypeClass = new JButton("...");
 		buttonChangeTypeClass.addActionListener(this);
@@ -158,7 +177,7 @@ public class AgentTypeGeneral extends AbstractPanel implements ActionListener
 		attributePanel.setBackground(Color.WHITE);
 		
 		// prepare Main-Panel
-		attributePanel.add(new JLabel(ImageIconLoader.scaleImageIcon(model.getIcon(), 32, 32)), new GridBagConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(5,5,5,5), 0, 0));
+		attributePanel.add(iconLabel, new GridBagConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(5,5,5,5), 0, 0));
 		attributePanel.add(buttonChangeImage, new GridBagConstraints(0, 1, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(5,5,5,5), 0, 0));
 		attributePanel.add(new JLabel("<html><h2>General Settings</h2>Here, you see all the main-settings for this AgentType.</html>"), new GridBagConstraints(1, 0, 2, 2, 1, 0, GridBagConstraints.LINE_START, GridBagConstraints.HORIZONTAL, new Insets(5,5,5,5), 0, 0));
 
@@ -167,7 +186,8 @@ public class AgentTypeGeneral extends AbstractPanel implements ActionListener
 		attributePanel.add(buttonChangeSourcePath, new GridBagConstraints(2, 2, 1, 1, 0, 0, GridBagConstraints.LINE_START, GridBagConstraints.HORIZONTAL, new Insets(5,5,5,5), 0, 0));
 
 		attributePanel.add(new JLabel("Source-File:"), new GridBagConstraints(0, 3, 1, 1, 0, 0, GridBagConstraints.LINE_START, GridBagConstraints.NONE, new Insets(5,5,5,5), 0, 0));
-		attributePanel.add(new JLabel(model.getDocument().getSourceName()), new GridBagConstraints(1, 3, 2, 1, 1, 0, GridBagConstraints.LINE_START, GridBagConstraints.NONE, new Insets(5,5,5,5), 0, 0));
+		attributePanel.add(textFieldSourceName, new GridBagConstraints(1, 3, 1, 1, 1, 0, GridBagConstraints.LINE_START, GridBagConstraints.NONE, new Insets(5,5,5,5), 0, 0));
+        attributePanel.add(buttonChangeSourceName, new GridBagConstraints(2, 3, 1, 1, 0, 0, GridBagConstraints.LINE_START, GridBagConstraints.HORIZONTAL, new Insets(5,5,5,5), 0, 0));
 
 		attributePanel.add(new JLabel("Type-Name:"), new GridBagConstraints(0, 4, 1, 1, 0, 0, GridBagConstraints.LINE_START, GridBagConstraints.NONE, new Insets(5,5,5,5), 0, 0));
 		attributePanel.add(textFieldTypeName, new GridBagConstraints(1, 4, 2, 1, 1, 0, GridBagConstraints.LINE_START, GridBagConstraints.NONE, new Insets(5,5,5,5), 0, 0));
@@ -188,19 +208,78 @@ public class AgentTypeGeneral extends AbstractPanel implements ActionListener
 		return attributePanel;
 	}
 
+	private void applyChanges()
+	{
+		String dialogMessage = "";
+
+		// check if type-name or type-package have changed
+		// if the type-name has changed automatically also change the source-name
+		if (!model.getName().equals(textFieldTypeName.getText()) ||
+			!model.getPackageName().equals(textFieldTypePackage.getText()))
+		{
+			dialogMessage+="<h3>Changing the name or package of an AgentType leads to a new source-name or -path!</h3><p><i>The names and locations of description-files are constructed out of the AgentType-name and -package,<br>changing the name or package also changes the name or path of the source-file.</i><p>&nbsp;";
+			textFieldSourceName.setText(textFieldTypeName.getText() + ".agent.xml");
+			model.setName(textFieldTypeName.getText());
+			model.setPackageName(textFieldTypePackage.getText());
+		}
+
+		// check if source-name or source-path have changed
+		// if the source-name has changed automatically also change the type-name
+		if (!model.getDocument().getSourceName().equals(textFieldSourceName.getText()) ||
+			!model.getDocument().getSourcePath().equals(textFieldSourcePath.getText()))
+		{
+			dialogMessage+="<h3>Changing the sourcepath or -file of an AgentType leads to a new AgentType-name or -package!</h3><p><i>The AgentType-name has to match the sourcefile-name (+ '.agent.xml')<br>and the package has to match the location of this sourcefile within the file-system.</i>";
+			textFieldTypeName.setText(textFieldSourceName.getText().substring(0, textFieldSourceName.getText().indexOf(".agent.xml")));
+			model.setName(textFieldTypeName.getText());
+			model.getDocument().setSourcePath(textFieldSourcePath.getText());
+			model.getDocument().setSourceName(textFieldSourceName.getText());
+		}
+
+		if (!dialogMessage.equals(""))
+			JOptionPane.showMessageDialog(parentFrame, "<html>"+dialogMessage+"<p>&nbsp;<p>Please make sure to save the AgentType, so that the new file will be created.</html>");
+
+		model.setClassName(textFieldTypeClass.getText());
+		model.setDescription(textAreaDescription.getText());
+		model.setPlatformType((String)comboBoxPlatform.getSelectedItem());
+		model.setIconName(iconName);
+	}
+
 	public void actionPerformed(ActionEvent evt)
 	{
-		if (evt.getSource() == buttonStart)
+		if (evt.getSource() == buttonChangeImage)
+		{
+			String fileName = (String)mainPanel.showDialog(AbstractMainPanel.CHOOSE_ICON_DIALOG);
+			iconName = fileName;
+			iconLabel.setIcon(ImageIconLoader.createImageIcon(iconName, 32, 32));
+		}
+		else if (evt.getSource() == buttonChangeSourcePath)
+		{
+			String directoryName = (String)mainPanel.showDialog(AbstractMainPanel.CHOOSE_DIRECTORY_DIALOG) + File.separator;
+			textFieldSourcePath.setText(directoryName);
+		}
+		else if (evt.getSource() == buttonChangeSourceName)
+		{
+			String name = (String)mainPanel.showDialog(AbstractMainPanel.CHOOSE_AGENTTYPE_FILE_DIALOG);
+			textFieldSourceName.setText(name);
+		}
+		else if (evt.getSource() == buttonChangeTypeClass)
+		{
+			String name = (String)mainPanel.showDialog(AbstractMainPanel.CHOOSE_JAVA_FILE_DIALOG);
+			name = name.substring(0, name.lastIndexOf("."));
+			textFieldTypeClass.setText(name);
+		}
+		else if (evt.getSource() == buttonStart)
 		{
             mainPanel.showDialog(new StartAgentInstanceDialog(mainPanel, model));
 		}
 		else if (evt.getSource() == buttonApply)
 		{
-            System.err.println("AgentTypeGeneral.actionPerformed: Apply Changes, implement me !!!");
+			applyChanges();
 		}
 		else if (evt.getSource() == buttonSave)
 		{
-            System.err.println("AgentTypeGeneral.actionPerformed: Save AgentType, implement me !!!");
+            applyChanges();
+			System.err.println("AgentTypeGeneral.actionPerformed: Save AgentType, implement me !!!");
 		}
 
 	}
