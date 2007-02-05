45358adc690c32789fc24cae4a02e9c0ba8de8fb
ridolfi <ridolfi@815794fe-97f2-0310-9206-e62f04a60ddc>
2007-02-05 09:46:35 +0000

    Constants for wsdl generation
    git-svn-id: https://jade.tilab.com/svn/jade_add-ons/trunk@974 815794fe-97f2-0310-9206-e62f04a60ddc

---
+++ wsig/src/com/whitestein/wsig/ws/WSDLConstants.java
@@ -0,0 +1,18 @@
+package com.whitestein.wsig.ws;
+
+public class WSDLConstants {
+	public static final String xsd = "http://www.w3.org/2001/XMLSchema";
+	public static final String wsdlsoap = "http://schemas.xmlsoap.org/wsdl/soap/";
+	public static final String bodyEncodingStyle = "http://schemas.xmlsoap.org/soap/encoding/";
+	public static final String transportURI = "http://schemas.xmlsoap.org/soap/http";
+	public static final String soapStyle = "rpc";
+	public static final String publishSoapBinding = "publishSoapBinding";
+	public static final String portPublish = "publish";
+	public static final String locationURI = "http://localhost:8080/wsig/publish";
+	public static final String QNameOperation = "operation";
+	public static final String soapActionURI = "soapActionURI";
+	public static final String QNamebody = "body";
+	public static final String bodyInputUse = "encoded";
+	public static final String outPart = "out";
+	public static final String part = "par";
+}
