100efab16f1382da8e9d5a4044a495861a056326
marchett <marchett@815794fe-97f2-0310-9206-e62f04a60ddc>
2007-02-12 13:59:45 +0000

    Changed ; in :
    git-svn-id: https://jade.tilab.com/svn/jade_add-ons/trunk@978 815794fe-97f2-0310-9206-e62f04a60ddc

--- wsig/misc/wsigcp.sh
+++ wsig/misc/wsigcp.sh
@@ -8,5 +8,7 @@ AC=../lib/commons-logging-1.1.jar:../lib/commons-discovery-0.2.jar
 GW=../dist/wsig.jar
 XI=../lib/xercesImpl-2.8.1.jar
 
-WSIGCP=$XI:$GW:$LOG4J:$AC:$AXIS:$UDDI4j:$WSDL
+XSD=../lib/org.eclipse.emf.common_2.3.0.v200612211251.jar:../lib/org.eclipse.emf.ecore.xmi_2.3.0.v200612211251.jar:../lib/org.eclipse.emf.ecore_2.3.0.v200612211251.jar:../lib/org.eclipse.emf_2.2.0.v200612211251.jar:../lib/org.eclipse.xsd_2.3.0.v200612211251.jar:
+
+WSIGCP=$XI:$GW:$LOG4J:$AC:$AXIS:$UDDI4j:$WSDL:$XSD
 
