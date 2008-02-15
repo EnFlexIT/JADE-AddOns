b8bdd32224bcae023848f776bc7d3381dc56af11
trucco <trucco@815794fe-97f2-0310-9206-e62f04a60ddc>
2008-02-15 17:21:41 +0000

    added javadoc
    git-svn-id: https://jade.tilab.com/svn/jade_add-ons/trunk@1155 815794fe-97f2-0310-9206-e62f04a60ddc

--- jade4android/src/jade/util/Logger.java
+++ jade4android/src/jade/util/Logger.java
@@ -6,44 +6,111 @@ import jade.util.leap.Properties;
 import jade.util.leap.Serializable;
 import android.util.Log;
 
+
+/**
+ * This class provides an implementation of the JADE logging API 
+ * to print out messages to a log file on the Android emulator.
+ * 
+ * According to java logging philosophy, the JADE logging API allows to set several logging levels.
+ * The levels in descending order are: <p>
+ * SEVERE (highest value) <br>
+ * WARNING <br>
+ * INFO <br>
+ * CONFIG <br>
+ * FINE <br>
+ * FINER <br>
+ * FINEST (lowest value)
+ * 
+ * Since the Android logging API defines different logging levels, the following 
+ * mapping rules have been followed:
+ * <p> SEVERE corresponds to <code>android.util.Log.ERROR</code><br>
+ * <p> WARNING corresponds to <code>android.util.Log.WARN</code><br>
+ * <p> INFO corresponds to <code>android.util.Log.INFO</code><br>
+ * <p> CONFIG corresponds to <code>android.util.Log.VERBOSE</code><br>
+ * <p> FINE, FINER and FINEST correspond to <code>android.util.Log.DEBUG</code><br>
+ * 
+ * <p> In addition, there is a level OFF that can be used to turn off logging, 
+ * and a level ALL that can be used to enable logging of all messages.
+ * <p>
+ * <p>
+ * For instance, in order to log the warning message  "Attention!", the
+ * following code should be used: <br><br>
+ *
+ * <code>Logger logger = Logger.getMyLogger(this.getClass().getName());</code><br>
+ * <code>if (logger.isLoggable(logger.WARNING)) </code>
+ * <br> <code>logger.log(Logger.WARNING,"Attention!"); </code>
+ * <p>
+ * Notice that the test <code>isLoggable</code> allows just to improve performance, but
+ * it has no side-effect.
+ *
+ * Following the Android logging style, the Logger class name is used as TAG and  the name 
+ * of the Logger is added to the printed message.
+ * @author Tiziana Trucco - Telecom Italia
+ * @author ...
+ */
+
 public class Logger implements Serializable{
 
-	//SEVERE is a message level indicating a serious failure.
+	/**
+	 * SEVERE is a message level indicating a serious failure.
+	 */
 	public static final int SEVERE	=	10;
-	//WARNING is a message level indicating a potential problem.
+	/**
+	 * WARNING is a message level indicating a potential problem.
+	 */
 	public static final int WARNING	=	9;
-	//INFO is a message level for informational messages
+	/**
+	 * INFO is a message level for informational messages
+	 */
 	public static final int INFO	=	8;
-	//CONFIG is a message level for static configuration messages.
+	/**
+	 * CONFIG is a message level for static configuration messages.
+	 */
 	public static final int CONFIG	=	7;
-	//FINE is a message level providing tracing information.
+	/**
+	 * FINE is a message level providing tracing information.
+	 */
 	public static final int FINE	=	5;
-	//FINER indicates a fairly detailed tracing message.
+	/**
+	 * FINER indicates a fairly detailed tracing message.
+	 */
 	public static final int FINER	=	4;
-	//FINEST indicates a highly detailed tracing message
+	/**
+	 * FINEST indicates a highly detailed tracing message
+	 */
 	public static final int FINEST	=	3;
-	//ALL indicates that all messages should be logged.
+	/**
+	 * ALL indicates that all messages should be logged. 
+	 */
 	public static final int ALL		=	-2147483648;
-	//Special level to be used to turn off logging
+	/**
+	 * Special level to be used to turn off logging
+	 */
 	public static final int OFF		=	2147483647;
-
-	//private static int MAX_TAG_LENGTH = 23; 
 	
 	private static Hashtable logManager = new Hashtable();
 	private String name;
 	
+	
+	/**
+	 * Private method to construct a logger for a named subsystem.
+	 * @param name A name for the logger
+	 */
 	private Logger(String name){
 		this.name = name;
 	}
 
 	public static void println(String log) {
-		//FIXME: name dovrebbe essere static
-		Log.println(Log.ASSERT, "", log);
+		Log.println(Log.ASSERT, Logger.class.getName(), log);
 	}
 	
+	/**
+	 * Find or create a logger for a named subsystem.
+	 * @param name The name of the logger.
+	 * @return the instance of the Logger.
+	 */
 	public synchronized static Logger getMyLogger(String name){
 		
-		//String value = ((name != null) ? ((name.length() > MAX_TAG_LENGTH) ? name.substring(0,MAX_TAG_LENGTH) : name) : "jade.util.Logger");
 		Object myLogger = logManager.get(name);
 		if (myLogger == null){
 			myLogger = new Logger(name);
@@ -52,13 +119,23 @@ public class Logger implements Serializable{
 		return (Logger)myLogger;
 	}
 	
+
+	/**
+	 * In this implementation this methods does nothing.
+	 * @param pp
+	 */
 	public static void initialize(Properties pp) {
-		//TODO	
+
 	}
 	
+	
+	/**
+	 * Checks if the current level is loggable
+	 * @param level
+	 * @return
+	 */
 	public boolean isLoggable(int level){
-		//FIXME: da capire come gestire OFF e ALL. dovrebbe esserci un valore SUPPRESS, ma non e' implementato al momento
-		
+		//FIXME: there should be a SUPPRESS level in android but at the moment it's not accessible.
 		if (level == OFF)
 			return false;
 		else if(level ==ALL)
@@ -67,31 +144,38 @@ public class Logger implements Serializable{
 			return Log.isLoggable(this.getClass().getName(), getAndroidLogLevel(level));
 	}
 	
+	/**
+	 * Prints out a log message onto the emulator log file.
+	 * @param level
+	 * @param msg
+	 */
+	
 	public void log(int level, String msg) {
-		//FIXME se level e' OFF ?
+		//FIXME OFF level not considered
 		Log.println(getAndroidLogLevel(level), this.getClass().getName(), name + ": " + msg);
 	}
 	
 	public void log(int level, String msg, Throwable t) {
-		//FIXME se level e' OFF  e ALL?
+		//FIXME OFF and ALL level not considered  
 		int ll = getAndroidLogLevel(level);
 		switch(ll){
-			case  Log.ERROR: 
-				Log.e(this.getClass().getName(), name + ": " + msg, t);
-				break;
-			case Log.WARN:
-				Log.w(this.getClass().getName(), name + ": " + msg, t);
-				break;
-			case Log.INFO:
-				Log.i(this.getClass().getName(), name + ": " + msg, t);
-				break;
-			case Log.VERBOSE:
-				Log.v(this.getClass().getName(), name + ": " + msg, t);
-				break;
-			case Log.DEBUG:
-				Log.d(this.getClass().getName(), name + ": " + msg, t);
-				break;
+		case  Log.ERROR: 
+			Log.e(this.getClass().getName(), name + ": " + msg, t);
+			break;
+		case Log.WARN:
+			Log.w(this.getClass().getName(), name + ": " + msg, t);
+			break;
+		case Log.INFO:
+			Log.i(this.getClass().getName(), name + ": " + msg, t);
+			break;
+		case Log.VERBOSE:
+			Log.v(this.getClass().getName(), name + ": " + msg, t);
+			break;
+		case Log.DEBUG:
+			Log.d(this.getClass().getName(), name + ": " + msg, t);
+			break;
 		}
+		
 	}
 	
 	private int getAndroidLogLevel(int level){
