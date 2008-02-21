6c585612c028995f69bd3ea43a1a67453c258a37
caire <caire@815794fe-97f2-0310-9206-e62f04a60ddc>
2008-02-21 14:06:00 +0000

    New version provided by the CASCOM project
    git-svn-id: https://jade.tilab.com/svn/jade_add-ons/trunk@1055 815794fe-97f2-0310-9206-e62f04a60ddc

--- BEFipaMessage/src/examples/DummyDecoder.java
+++ BEFipaMessage/src/examples/DummyDecoder.java
@@ -1,74 +1,64 @@
-/*====================================================================
- 
-    Open Source Copyright Notice and License: FIPA Message 
-    Representation in Bit-Efficient Encoding
- 
-     1.   The  programs and other works made available to you in
-          these files ("the  Programs")  are  Copyright (c) 2001
-          Sonera  Corporation,  Teollisuuskatu 15, P.O. Box 970,
-          FIN-00051 SONERA, Finland.  
-          All rights reserved.
-
-    2.    Your   rights  to  copy,  distribute  and  modify  the
-          Programs are as set out in the Sonera Public  License,
-          a  copy  of  which  can be found in file "LICENSE". By
-          downloading the  files  containing  the  Programs  you
-          accept the terms and conditions of the Public License.
-          You do not have to accept these terms and  conditions,
-          but  unless  you  do  so you have no rights to use the
-          Programs.
-
-    The  Original  Code is an implementation of the FIPA Message
-    Representation in Bit-Efficient Encoding.
-
-    The  Initial  Developer  of  the  Original  Code  is  Sonera
-    Corporation. Portions created by Sonera Corporation  or  its
-    subsidiaries  are  Copyright  (c)  Sonera  Corporation.  
-    All Rights Reserved.
-
-    Contributor(s):
-     (add contributor names here)
-
-====================================================================*/
+/**
+ * ***************************************************************
+ * JADE - Java Agent DEvelopment Framework is a framework to develop
+ * multi-agent systems in compliance with the FIPA specifications.
+ * Copyright (C) 2000 CSELT S.p.A.
+ * 
+ * GNU Lesser General Public License
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation,
+ * version 2.1 of the License.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307, USA.
+ * **************************************************************
+ */
+package examples;
 import java.io.*;
 import java.util.*;
-import sonera.fipa.acl.*;
 import jade.lang.acl.*;
-import jade.util.Logger;
+import java.lang.Integer;
+import cascom.fipa.acl.ACLInputStream;
 
-public class DummyDecoder {
-        
-        private static Logger logger = Logger.getMyLogger(DummyDecoder.class.getName());
-        
-        public static void main (String[] args) {
+/**
+ *  Example class for using bit-efficient ACL decoder.
+ */
+public class DummyDecoder {        
+    public static void main(String[] args) {
                 /*
-		 * Initialize Bit-efficient ACL input stream.
-	 	 */
-                ACLInputStream in = null;
-                if (args.length!=0) {
-                        in = new ACLInputStream(System.in, new Integer(args[0]).intValue());
-                } else {
-                        in = new ACLInputStream(System.in);
-                }
-                int c = 0;
-                try {
-                        while (true) {
-                                /*
-				 * Read a bit-efficiently coded message
-				 */
-                                ACLMessage m = in.readMsg();
-                                /*
-				 * And dump it to stdout
-				 */
-                                System.out.println(m.toString());
-                                ++c;
-                        }
-                } catch (Exception e) {
-                		if(logger.isLoggable(Logger.WARNING))
-                			logger.log(Logger.WARNING,e.getMessage());
-                        e.printStackTrace();
-                }
-                if(logger.isLoggable(Logger.INFO))
-                	logger.log(Logger.INFO,c+ " message(s) parsed");
+                 * Initialize Bit-efficient ACL input stream.
+                 */
+        ACLInputStream in = null;
+        if (args.length!=0) {
+            in = new ACLInputStream(System.in, new Integer(args[0]).intValue());
+        } else {
+            in = new ACLInputStream(System.in);
         }
+        int c = 0;
+        try {
+            while (true) {
+                                /*
+                                 * Read a bit-efficiently coded message
+                                 */
+                ACLMessage m = in.readMsg();
+                                /*
+                                 * And dump it to stdout
+                                 */
+                System.out.println(m.toString());
+                ++c;
+            }
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
+        System.err.println("INFO"+c+ " message(s) parsed");
+    }
 }
--- BEFipaMessage/src/examples/DummyEncoder.java
+++ BEFipaMessage/src/examples/DummyEncoder.java
@@ -1,90 +1,82 @@
-/*====================================================================
- 
-    Open Source Copyright Notice and License: FIPA Message 
-    Representation in Bit-Efficient Encoding
- 
-     1.   The  programs and other works made available to you in
-          these files ("the  Programs")  are  Copyright (c) 2001
-          Sonera  Corporation,  Teollisuuskatu 15, P.O. Box 970,
-          FIN-00051 SONERA, Finland.  
-          All rights reserved.
-
-    2.    Your   rights  to  copy,  distribute  and  modify  the
-          Programs are as set out in the Sonera Public  License,
-          a  copy  of  which  can be found in file "LICENSE". By
-          downloading the  files  containing  the  Programs  you
-          accept the terms and conditions of the Public License.
-          You do not have to accept these terms and  conditions,
-          but  unless  you  do  so you have no rights to use the
-          Programs.
-
-    The  Original  Code is an implementation of the FIPA Message
-    Representation in Bit-Efficient Encoding.
-
-    The  Initial  Developer  of  the  Original  Code  is  Sonera
-    Corporation. Portions created by Sonera Corporation  or  its
-    subsidiaries  are  Copyright  (c)  Sonera  Corporation.  
-    All Rights Reserved.
-
-    Contributor(s):
-     (add contributor names here)
-
-====================================================================*/
+/**
+ * ***************************************************************
+ * JADE - Java Agent DEvelopment Framework is a framework to develop
+ * multi-agent systems in compliance with the FIPA specifications.
+ * Copyright (C) 2000 CSELT S.p.A.
+ * 
+ * GNU Lesser General Public License
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation,
+ * version 2.1 of the License.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307, USA.
+ * **************************************************************
+ */
+package examples;
 import java.io.*;
-import sonera.fipa.acl.*;
 import jade.lang.acl.*;
 import jade.core.*;
-import jade.util.Logger;
+import java.lang.Integer;
+import cascom.fipa.acl.ACLOutputStream;
 
-public class DummyEncoder {
-        
-        private static Logger logger = Logger.getMyLogger(DummyEncoder.class.getName());
-        
-        public static void main (String[] args) {
-                int c = 0;
-                ACLMessage aclMessage[] = new ACLMessage[100];
+/**
+ *  Example class for using bit-efficient ACL encoder.
+ */
+public class DummyEncoder {        
+    public static void main(String[] args) {
+        int c = 0;
+        ACLMessage aclMessage[] = new ACLMessage[100];
                 /*
-		 * We are reading from standard input
-		 */
-                BufferedReader in =
-                        new BufferedReader(new InputStreamReader(System.in));
-                String line = null;
+                 * We are reading from standard input
+                 */
+        BufferedReader in =
+                new BufferedReader(new InputStreamReader(System.in));
+        String line = null;
                 /*
-	 	 * Initialize the ACL OutputStream
-	 	 */
-                ACLOutputStream os = null;
-                if (args.length!=0) {
-                        os = new ACLOutputStream(System.out,
-                                new Integer(args[0]).intValue());
-                } else {
-                        os = new ACLOutputStream(System.out);
-                }
-                StringACLCodec sc = new StringACLCodec(in, null);
-                try {
-                        while(true) {
-                                ACLMessage m = sc.decode();
-//				byte [] __o = new byte[1800];
-//				m.setContentObject(__o);
-                                if(logger.isLoggable(Logger.INFO))
-                                	logger.log(Logger.INFO,m.toString());
-                                os.write(m);
-                                ++c;
-                        }
-                } catch (Exception e) {
-                        if(logger.isLoggable(Logger.WARNING))
-                        	logger.log(Logger.WARNING,"-- Error: "+e);
-                }
-                                /*
-				 * Here we assume that whole message is
-				 * given in one line; stupid assumption,
-				 * but better than nothing...
-				 */
-                                /*
-				 * If we succesfully parsed the message,
-				 * then we can write that to stdout
-				 * using bit-efficient encoding.
-				 */
-                if(logger.isLoggable(Logger.INFO))
-                	logger.log(Logger.INFO,c+" message(s) written to stdout");
+                 * Initialize the ACL OutputStream
+                 */
+        ACLOutputStream os = null;
+        if (args.length!=0) {
+            os = new ACLOutputStream(System.out,
+                    new Integer(args[0]).intValue());
+        } else {
+            os = new ACLOutputStream(System.out);
         }
+        StringACLCodec sc =new StringACLCodec(in, null);
+        
+        try {
+            while(true) {
+                ACLMessage m = sc.decode();
+                byte [] __o = new byte[1800];
+                m.setContentObject(__o);
+                System.err.println("INFO"+m.toString());
+                os.write(m);
+                ++c;
+            }
+        } catch (Exception e) {
+            
+            System.err.println("-- Error: "+e);
+        }
+                                /*
+                                 * Here we assume that whole message is
+                                 * given in one line; stupid assumption,
+                                 * but better than nothing...
+                                 */
+                                /*
+                                 * If we succesfully parsed the message,
+                                 * then we can write that to stdout
+                                 * using bit-efficient encoding.
+                                 */
+        System.err.println("INFO"+c+" message(s) written to stdout");
+    }
 }
