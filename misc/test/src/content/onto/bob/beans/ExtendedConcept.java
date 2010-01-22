7d047281d0db0540f0baae1380f7e8852ea4d90c
scagliotti <scagliotti@815794fe-97f2-0310-9206-e62f04a60ddc>
2010-01-22 16:33:14 +0000

    Removed BOB test
    git-svn-id: https://jade.tilab.com/svn/jade_add-ons/trunk@1736 815794fe-97f2-0310-9206-e62f04a60ddc

--- misc/test/src/content/onto/bob/beans/ExtendedConcept.java
+++
@@ -1,7 +0,0 @@
-package content.onto.bob.beans;
-
-import jade.content.Concept;
-
-public interface ExtendedConcept extends Concept {
-
-}
--- misc/test/startTest.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-SETLOCAL
-
-ant run
-
-ENDLOCAL
