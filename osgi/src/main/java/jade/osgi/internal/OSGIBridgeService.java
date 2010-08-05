f29f41ef6ad968ce791560e048d79e4b8f48301f
caire <caire@815794fe-97f2-0310-9206-e62f04a60ddc>
2010-08-05 13:11:10 +0000

    Minor modification
    git-svn-id: https://jade.tilab.com/svn/jade_add-ons/trunk@1786 815794fe-97f2-0310-9206-e62f04a60ddc

--- osgi/src/main/java/jade/osgi/internal/OSGIBridgeService.java
+++ osgi/src/main/java/jade/osgi/internal/OSGIBridgeService.java
@@ -17,13 +17,11 @@ public class OSGIBridgeService extends BaseService {
 
 	public static final String NAME = OSGIBridgeHelper.SERVICE_NAME;
 
-	private AgentManager agentManager;
 	private CommandIncomingFilter incomingFilter;
 
 	public void init(AgentContainer ac, Profile p) throws ProfileException {
 		super.init(ac, p);
 		incomingFilter = new CommandIncomingFilter();
-		agentManager = JadeActivator.getInstance().getAgentManager();
 	}
 
 	public String getName() {
@@ -31,7 +29,7 @@ public class OSGIBridgeService extends BaseService {
 	}
 	
 	public ServiceHelper getHelper(Agent a) throws ServiceException {
-		return new OSGIBridgeHelperImpl(agentManager);
+		return new OSGIBridgeHelperImpl(JadeActivator.getInstance().getAgentManager());
 	}
 	
 	public Filter getCommandFilter(boolean direction) {
@@ -58,7 +56,7 @@ public class OSGIBridgeService extends BaseService {
 		private void handleInformKilled(VerticalCommand cmd) {
 			Object[] params = cmd.getParams();
 			AID aid = (AID) params[0];
-			agentManager.removeAgent(aid);
+			JadeActivator.getInstance().getAgentManager().removeAgent(aid);
 		}
 	}
 	
