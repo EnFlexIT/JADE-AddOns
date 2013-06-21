6b13015a47aab67a86139eb86a6ef11fbdf295b8
scagliotti <scagliotti@815794fe-97f2-0310-9206-e62f04a60ddc>
2013-06-21 14:06:48 +0000

    Removed log4j dependency
    git-svn-id: https://jade.tilab.com/svn/jade_add-ons/trunk@1981 815794fe-97f2-0310-9206-e62f04a60ddc

--- dynamicClient/bin/DynamicClientShell.sh
+++ dynamicClient/bin/DynamicClientShell.sh
@@ -5,7 +5,7 @@ JADE_HOME=../../..
 
 ant -f %WSDC_HOME%\build.xml lib
 
-WSDC_LIB=${JADE_HOME}/lib/jade.jar:${WSDC_HOME}/lib/wsdc.jar:${WSDC_HOME}/lib/axisPlus.jar:${WSDC_HOME}/lib/commons-discovery-0.2.jar:${WSDC_HOME}/lib/commons-logging-1.1.1.jar:${WSDC_HOME}/lib/jaxrpc-1.1.jar:${WSDC_HOME}/lib/log4j-1.2.8.jar:${WSDC_HOME}/lib/saaj-1.2.jar:${WSDC_HOME}/lib/wsdl4j-1.5.1.jar:${WSDC_HOME}/lib/jadeMisc.jar:${WSDC_HOME}/lib/wss4j-1.5.1.jar:${WSDC_HOME}/lib/xmlsec-1.3.0.jar:.
+WSDC_LIB=${JADE_HOME}/lib/jade.jar:${WSDC_HOME}/lib/wsdc.jar:${WSDC_HOME}/lib/axisPlus.jar:${WSDC_HOME}/lib/commons-discovery-0.2.jar:${WSDC_HOME}/lib/commons-logging-1.1.1.jar:${WSDC_HOME}/lib/jaxrpc-1.1.jar:${WSDC_HOME}/lib/saaj-1.2.jar:${WSDC_HOME}/lib/wsdl4j-1.5.1.jar:${WSDC_HOME}/lib/jadeMisc.jar:${WSDC_HOME}/lib/wss4j-1.5.1.jar:${WSDC_HOME}/lib/xmlsec-1.3.0.jar:.
 
 SAVECLASSPATH=${CLASSPATH}
 
